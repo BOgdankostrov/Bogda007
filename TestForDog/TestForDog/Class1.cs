bb97a0f8b9715ad26600150d03a81ab3731ffb56
bogda <bogda@DESKTOP-9JO2CCN>
2017-10-17 01:40:07 +0300

    гшр

--- TestForDog/TestForDog/Class1.cs
+++ TestForDog/TestForDog/Class1.cs
@@ -7,6 +7,5 @@ using System.Threading.Tasks;
 namespace TestForDog
 {
     public class Class1
-    {
-    }
+    {    }
 }
--- TestForDog/TestForGods/Question.cs
+++ TestForDog/TestForGods/Question.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace TestForGods
 {
-    class TestViewModel
+    class Question
     {
-
     }
 }
