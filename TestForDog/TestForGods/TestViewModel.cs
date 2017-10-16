43c52e571f11c480b765222e42266903a52a58ef
Ольга Зайцева <olya9771@gmail.com>
2017-10-17 01:34:59 +0300

    kaka

--- TestForDog/TestForGods/TestViewModel.cs
+++ TestForDog/TestForGods/TestViewModel.cs
@@ -1,12 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestForGods
 {
-    class TestViewModel
+    
+    class TestViewModel : INotifyPropertyChanged
     {
+        const int QUESTIONSAMOUNT=10;
+        private Question currentquestion;
+        private List<Question> questions;
+       private int index=0;
+
+        int countTrueAnser = 0;
+
+
+          
+        public TestViewModel()
+        {
+            
+            questions = new List<Question>();
+            move();
+
+        }
+         public void move()
+        {
+            index++;
+            currentquestion = questions[index];
+
+        }
+         
+        public void choose(int i)
+        {
+            if(currentquestion.)
+        }
+        public Question Question
+        {
+            get
+            {
+                return currentquestion;
+            }
+            set
+            {
+                this.currentquestion = value;
+                DoPropertyChanged("Question");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void DoPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
