05bf4baf2d5f315d5b68a9e1cc15ad02d1ad31d7
bogda <bogda@DESKTOP-9JO2CCN>
2017-10-17 03:37:02 +0300

    ыфаа

---
+++ ConsoleApplication1/ConsoleApplication1/Option.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Option : INotifyPropertyChanged
+    {
+        private string value;
+        private bool win;
+        private bool visible;
+        private string explanation;
+        public Option(string value, string expanation, bool win)
+        {
+            this.Value = value;
+            this.explanation = explanation;
+            this.win = win;
+            visible = false;
+        }
+        public bool Win
+        {
+            get
+            {
+                return win;
+            }
+            set
+            {
+                this.win = value;
+            }
+        }
+        public bool Visible
+        {
+            get
+            {
+                return visible;
+            }
+            set
+            {
+                this.visible = value;
+            }
+        }
+        public string Explanation
+        {
+            get
+            {
+                return explanation;
+            }
+            set
+            {
+                this.explanation = value;
+            }
+        }
+        public string Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void DoPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fffff gg = new ConsoleApplication1.fffff();
+            for (int i = 0; i < gg.ListOfAsk.Count; i++)
+            {
+
+                Console.WriteLine(gg.ListOfAsk[i].question);
+            }
+        }
+    }
+}
